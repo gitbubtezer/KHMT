5640425ef5fa43c480eba0d9fc529a2bf1a4fbda
P_Son <86548421+gitbubtezer@users.noreply.github.com>
2023-06-04 10:54:04 +0700

    Create Buoi1.py

---
+++ B1/Buoi1.py
@@ -0,0 +1,25 @@
+print("Hello world!")
+
+name = "Pham Son"
+#print(name)
+
+#Câu 1: Các kiểu dữ liệu: Int, float, long, double, string, boolean.
+#Câu 2: Qua trình biên dich 
+#Câu 3: C
+#Câu 4: A
+
+name = input("Input your name: ")
+print("Welcome, ",name)
+
+fname = input("First name: ")
+lname = input("Last name: ")
+pnum = input("Phone number: ")
+
+print("Your registered name is ", fname , " ",lname)
+print("Your phone number is ",pnum)
+
+print("Name\t\t:\tMichael")
+print("Birthdate\t:\t01/01/2022")
+
+year = input("What year is it?")
+print("HAPPY NEW YEAR\n!!! ",year," !!!")
